db47417e0e8189d10667ad6cf9e8d59a333b35b0
Abid Al-Wasi <abidalwasi@outlook.com>
2021-02-10 23:44:43 +0600

    updated project

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = ({title}) => {
   return (
     <header className="header">
       <h1 style={headingStyle}>{title}</h1>
-      <Button color="red" text="Add"/>
+      <Button color="black" text="Add"/>
     </header>
   )
 }
