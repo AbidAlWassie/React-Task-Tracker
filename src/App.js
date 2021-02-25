0f74046a38e951ddbc28e75789213893133f7f5e
Abid Al-Wasi <abidalwasi@outlook.com>
2021-02-25 23:25:23 +0600

    updated project

--- src/App.js
+++ src/App.js
@@ -31,10 +31,15 @@ const App = () => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
   
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    console.log(id);
+  }
+  
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : "No task to show."}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : "No task to show."}
     </div>
   )
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,9 @@
 import { FaTimes } from "react-icons/fa"
 
-const Task = ({task, onDelete}) => {
+const Task = ({task, onDelete, onToggle}) => {
   return (
     <div className="task">
-      <h3>{task.text} <FaTimes style={{color: "red", cursor: "pointer"}} onClick={() => onDelete(task.id)} /></h3>
+      <h3>{task.text} <FaTimes style={{color: "red", cursor: "pointer"}} onClick={() => onDelete(task.id)} onToggle={onToggle} /></h3>
       <p>{task.day}</p>
     </div>
   )
