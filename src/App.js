a5a8812228a23e2a72e78e6b3ac367a9b134ff76
Abid Al-Wasi <abidalwasi@outlook.com>
2021-02-25 21:50:53 +0600

    updated project

--- src/App.js
+++ src/App.js
@@ -25,10 +25,16 @@ const App = () => {
       },
     ]
   )
+  
+  // Delete a Task
+  const deleteTask = (id) => {
+    console.log("delete", id)
+  }
+  
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   )
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 
-const Button = ( { color, text, btnClick} ) => {
+const Button = ({color, text, btnClick}) => {
   
   
   return (
