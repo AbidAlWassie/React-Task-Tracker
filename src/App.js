cce99e2d4549a50fc361445b1970871d6a05b8c1
Abid Al-Wasi <abidalwasi@outlook.com>
2021-02-25 22:11:30 +0600

    updated project

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : "No task to show."}
     </div>
   )
 }
