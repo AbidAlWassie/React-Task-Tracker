9546587245bfde878ce75099dcbfb8e7892885d1
Abid Al-Wasi <abidalwasi@outlook.com>
2021-02-26 13:57:26 +0600

    new component

---
+++ src/components/AddTask.js
@@ -0,0 +1,10 @@
+const AddTask = () => {
+  return (
+    <form className="add-form" action="">
+      <div className="form-control">
+        <label>Task</label>
+        <input type="text" placeholder="Add Task"/>
+      </div>
+    </form>
+  )
+}
