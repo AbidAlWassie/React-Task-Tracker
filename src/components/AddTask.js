ad911406331ba381204451b5053a25f70e7fbf31
Abid Al-Wasi <abidalwasi@outlook.com>
2021-02-28 20:15:49 +0600

    updated porject

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,11 +1,17 @@
+import {useState} from "React"
+
 const AddTask = () => {
+  const [text, setText] = useState("");
+  const [day, setDay] = useState("");
+  const [reminder, setReminder] = useState("false");
+  
   return (
     <form className="add-form" action="">
       <div className="form-control">
         <label>Task</label>
-        <input type="text" placeholder="Add Task"/>
+        <input type="text" placeholder="Add Task" value={text} onChange={(e) => setText(e.target.value)} />
       </div>
-  
+   
       <div className="form-control">
         <label>Day & Time</label>
         <input type="text" placeholder="Add Day & Time"/>
