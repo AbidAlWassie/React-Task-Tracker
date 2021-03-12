5749bd2d9fc5036a039dee5b7c46bedf60aaf885
Abid Al-Wasi <abidalwasi@outlook.com>
2021-03-12 22:40:23 +0600

    fixed add button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,25 @@
-import PropTypes from "prop-types";
+import PropTypes from "prop-types"
 
-const Button = ({color, text, btnClick}) => {
+const Button = ({ color, text, onClick }) => {
   return (
-  <button onClick={btnClick} style={{backgroundColor: color}} className="btn">{text}</button>
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className="btn"
+    >
+      {text}
+    </button>
   )
 }
 
 Button.defaultProps = {
-  color: "steelblue",
+  color: 'steelblue',
 }
 
 Button.propTypes = {
   text: PropTypes.string,
   color: PropTypes.string,
+  onClick: PropTypes.func,
 }
 
 export default Button;
